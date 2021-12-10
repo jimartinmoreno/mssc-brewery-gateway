410a53a30928824f6a272a33244461c21c9e80ea
jimartinmoreno <joseignacio.martinmoreno@gmail.com>
2021-12-10 17:08:48 +0100

    Docker compose

--- src/main/java/com/sfg/gateway/MsscBreweryGatewayApplication.java
+++ src/main/java/com/sfg/gateway/MsscBreweryGatewayApplication.java
@@ -2,8 +2,10 @@ package com.sfg.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsscBreweryGatewayApplication {
     public static void main(String[] args) {
         SpringApplication.run(MsscBreweryGatewayApplication.class, args);
